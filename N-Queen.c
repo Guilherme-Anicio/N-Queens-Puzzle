9c89d0ecbde340958d252f97428f601cd44e8b2f
Guilherme M Anicio <gui.anicio@gmail.com>
2023-05-21 21:25:31 -0300

    Now, a message will be printed if the problem has no solution

--- N-Queen.c
+++ N-Queen.c
@@ -6,8 +6,8 @@
 bool isSafe(bool board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
 
     //Check previous col
-    for(int i = 0; i < col; i++) {
-        if(board[row][i] == true){
+    for(int i = 0; i < row; i++) {
+        if(board[i][col] == true){
             return false;
         }
     }
@@ -19,26 +19,12 @@ bool isSafe(bool board[BOARD_SIZE][BOARD_SIZE], int row, int col) {
         }
     }
 
-    // Check lower left diagonal
-    for (int i = row, j = col; i < BOARD_SIZE && j >= 0; i++, j--) {
-        if (board[i][j] == true) {
-            return false;
-        }
-    }
-
-    /*//Check upper right diagonal
+    //Check upper right diagonal
     for (int i = row, j = col; i >= 0 && j < BOARD_SIZE; i--, j++) {
         if (board[i][j] == true) {
             return false;
         }
     }
-
-    // Check lower right diagonal
-    for (int i = row-1, j = col-1; i < BOARD_SIZE && j < BOARD_SIZE; i++, j++) {
-        if (board[i][j] == true) {
-            return false;
-        }
-    }*/
 
     return true;
 
@@ -59,24 +45,20 @@ void printResult(bool board[BOARD_SIZE][BOARD_SIZE]) {
 
 }
 
-bool solveNQueensProblem(bool board[BOARD_SIZE][BOARD_SIZE], int col) {
+bool solveNQueensProblem(bool board[BOARD_SIZE][BOARD_SIZE], int row) {
 
-    printResult(board);
-    printf("\n\n");
-
-    if(col >= BOARD_SIZE) {
-        printf("terminou\n\n");
+    if(row == BOARD_SIZE) {
         printResult(board);
         return true;
     }
 
-    for(int row = 0; row < BOARD_SIZE; row++) {
+    for(int col = 0; col < BOARD_SIZE; col++) {
 
         if(isSafe(board, row, col)) {
 
             board[row][col] = true;
 
-            if(solveNQueensProblem(board, col+1)) {
+            if(solveNQueensProblem(board, row+1)) {
                 return true;
             }
 
@@ -94,7 +76,11 @@ void main() {
 
     bool board[BOARD_SIZE][BOARD_SIZE] = {false};
 
-    solveNQueensProblem(board, 0);
+    bool result = solveNQueensProblem(board, 0);
+
+    if(!result) {
+        printf("This board has no solution.");
+    }
 
 }
 
